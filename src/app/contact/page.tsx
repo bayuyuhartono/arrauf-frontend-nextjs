a3f80bad5c0d4961a86d48a776e1fd0782626338
bayuyuhartono <bpyuhartono@pvg.co.id>
2024-11-16 00:28:05 +0700

    server.js

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,1 +1,7 @@
-<h2>Contact</h2>
+export default function Sd(){
+    return(
+        <div className="flex flex-col w-full h-full font-serif text-lg">
+            <h2>Contact</h2>
+        </div>
+    )
+}
